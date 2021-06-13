c35e778fe2591e3293ea86cbee6612b76438a9e6
iainbrux <me@iainbrux.com>
2021-06-14 00:30:16 +0100

    remove "Hello World" get request  -debug

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,10 +2,6 @@ const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
 notesRouter.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
-notesRouter.get("/", (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
